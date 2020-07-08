e4eb4ca12d8e85e2d40b6d8972340098f268f389
Natalia-77 <vlasnata80@gmail.com>
2020-07-08 08:28:38 +0300

    New

---
+++ Project5/Project5/Builder.cpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <Windows.h>
+#include <locale>;
+#include <string>
+#include <conio.h>
+#include <math.h>
+
+using namespace std;
+
+	
+	
