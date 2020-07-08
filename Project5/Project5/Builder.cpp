07567317840ea87b01169bd6fd5c4010dd467b19
Natalia-77 <vlasnata80@gmail.com>
2020-07-08 23:00:20 +0300

    Builder

--- Project5/Project5/Builder.cpp
+++ Project5/Project5/Builder.cpp
@@ -1,124 +1,27 @@
-#pragma once
-#include <iostream>
-#include <ctime>
-#include <Windows.h>
-#include <locale>;
-#include <string>
-#include <conio.h>
-#include <math.h>
-
-using namespace std;
-
-
-class Pizza
-{
-	string ingredients;
-public:
-	void AddIngredient(string text)
-	{
-		ingredients += (text + " ");
-		//ingredients += "\n";
-	}
-	void EatPizza()
-	{
-		cout << ingredients << endl;
-	}
-};
-
-
-class Cooker//---ABSTRACT BUILDER
-{
-public:
-	Cooker()
-	{
-		pizza = new Pizza();
-	}
-	Pizza* pizza;
-	virtual void AddSouse() = 0;
-	virtual void AddSousige() = 0;
-	virtual void AddCheese() = 0;
-	virtual void AddDough() = 0;
-	Pizza* GetPizza()
-	{
-		return pizza;
-	}
-};
-
-class CoolCooker : public Cooker//---CONCRETE BUILDER  1
-{
-public:
-	//CoolCooker(): Cooker()
-	//{	 }
-	void AddSouse()
-	{
-		pizza->AddIngredient("a lot of souse");
-	}
-	void AddSousige()
-	{
-		pizza->AddIngredient("basturma");
-	}
-	void AddCheese()
-	{
-		pizza->AddIngredient("blue cheese");
-	}
-	void AddDough()
-	{
-		pizza->AddIngredient("slim dough");
-	}
-};
-
-class PoorCooker : public Cooker//---CONCRETE BUILDER  2
-{
-	void AddSouse() override
-	{
-		pizza->AddIngredient(" ");
-	}
-	void AddSousige() override
-	{
-		pizza->AddIngredient("cheep sousige");
-	}
-	void AddCheese() override
-	{
-		pizza->AddIngredient("cheder");
-	}
-	void AddDough() override
-	{
-		pizza->AddIngredient("thin dough");
-	}
-};
-
-
-class Pizzerija//---DIRECTOR
-{
-public:
-	void CreateBigPizza(Cooker* cooker)
-	{
-		cooker->AddDough();
-		cooker->AddSouse();
-		cooker->AddCheese();
-		cooker->AddSousige();
-	}
-	void CreateSmallPizza(Cooker* cooker)
-	{
-		cooker->AddDough();
-		cooker->AddCheese();
-	}
-};
+#include "Builder.h"
 
 void main()
 {
-	Pizzerija pizzerija;
-	Cooker* cooker1 = new PoorCooker();
-	Cooker* cooker2 = new CoolCooker();
+	ChefManager manager;
+	Builder* builder1 = new StoneBuilder();
+	Builder* builder2 = new WoodBuilder();
 
-	pizzerija.CreateSmallPizza(cooker2);
+	manager.CreateBigVilla(builder2);
+	manager.CreateLittleHouse(builder1);
 
-	Pizza* pizza = cooker2->GetPizza();
-	pizza->EatPizza();
 
-	delete pizza;
-	delete cooker1;
-	delete cooker2;
+	House* house = builder2->GetHouse();
+	house->BuildOrder();
+
+    House* house2=builder1->GetHouse();
+	house2->BuildOrder();
+
+
+	delete house;
+	delete house2;
+	delete builder1;
+	delete builder2;
+	
 }
 
 	
