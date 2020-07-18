941a5127d0ca17b6444aa6c36cb6b98979b9e315
Natalia-77 <vlasnata80@gmail.com>
2020-07-18 22:42:32 +0300

    Wocabulary

--- Project5/Project5/Builder.cpp
+++ Project5/Project5/Builder.cpp
@@ -1,29 +1,29 @@
-#include "Builder.h"
-
-
-void main()
-{
-	ChefManager manager;
-	Builder* builder1 = new StoneBuilder();
-	Builder* builder2 = new WoodBuilder();
-
-	manager.CreateBigVilla(builder2);
-	manager.CreateLittleHouse(builder1);
-
-
-	House* house = builder2->GetHouse();
-	house->BuildOrder();
-
-    House* house2=builder1->GetHouse();
-	house2->BuildOrder();
-
-
-	delete house;
-	delete house2;
-	delete builder1;
-	delete builder2;
-	
-}
-
-	
-	
+//#include "Builder.h"
+//
+//
+//void main()
+//{
+//	ChefManager manager;
+//	Builder* builder1 = new StoneBuilder();
+//	Builder* builder2 = new WoodBuilder();
+//
+//	manager.CreateBigVilla(builder2);
+//	manager.CreateLittleHouse(builder1);
+//
+//
+//	House* house = builder2->GetHouse();
+//	house->BuildOrder();
+//
+//    House* house2=builder1->GetHouse();
+//	house2->BuildOrder();
+//
+//
+//	delete house;
+//	delete house2;
+//	delete builder1;
+//	delete builder2;
+//	
+//}
+//
+//	
+//	
--- Project5/Project5/Builder.h
+++ Project5/Project5/Builder.h
@@ -1,105 +1,105 @@
-#pragma once
-
-#include <iostream>
-#include <Windows.h>
-#include <string>
-#include <conio.h>
-using namespace std;
-
-
-
-class House
-{
-	string order;
-public:
-	void AddOrder(string text)
-	{
-		order += (text + " ");
-	}
-	void BuildOrder()
-	{
-		cout << order << endl;
-	}
-};
-
-
-
-class Builder
-{
-public:
-	Builder()
-	{
-		house = new House();
-	}
-	House* house;
-	virtual void AddColor() = 0;
-	virtual void AddMaterial() = 0;
-	virtual void AddFloor() = 0;
-	virtual void AddWindow() = 0;
-	House* GetHouse()
-	{
-		return house;
-	}
-};
-
-class WoodBuilder : public Builder
-{
-public:
-	void AddColor()
-	{
-	house->AddOrder("This is a white house");
-	}
-	void AddMaterial()
-	{
-		house->AddOrder("wood");
-	}
-	void AddFloor()
-	{
-		house->AddOrder("one-floor");
-	}
-	void AddWindow()
-	{
-		house->AddOrder("panoram window");
-	}
-};
-
-class StoneBuilder : public Builder
-{
-	void AddColor() override
-	{
-		house->AddOrder("This is grey house ");
-	}
-	void AddMaterial() override
-	{
-		house->AddOrder("stone");
-	}
-	void AddFloor() override
-	{
-		house->AddOrder("three-floor");
-	}
-	void AddWindow() override
-	{
-		house->AddOrder("enormous windows");
-	}
-};
-
-
-class ChefManager
-{
-public:
-	void CreateBigVilla(Builder* builder)
-	{
-		builder->AddColor();
-		builder->AddMaterial();
-		builder->AddFloor();
-		builder->AddWindow();
-	}
-	void CreateLittleHouse(Builder* builder)
-	{
-		builder->AddColor();
-		builder->AddFloor();
-		builder->AddMaterial();
-		builder->AddWindow();
-	}
-};
-
+//#pragma once
+//
+//#include <iostream>
+//#include <Windows.h>
+//#include <string>
+//#include <conio.h>
+//using namespace std;
+//
+//
+//
+//class House
+//{
+//	string order;
+//public:
+//	void AddOrder(string text)
+//	{
+//		order += (text + " ");
+//	}
+//	void BuildOrder()
+//	{
+//		cout << order << endl;
+//	}
+//};
+//
+//
+//
+//class Builder
+//{
+//public:
+//	Builder()
+//	{
+//		house = new House();
+//	}
+//	House* house;
+//	virtual void AddColor() = 0;
+//	virtual void AddMaterial() = 0;
+//	virtual void AddFloor() = 0;
+//	virtual void AddWindow() = 0;
+//	House* GetHouse()
+//	{
+//		return house;
+//	}
+//};
+//
+//class WoodBuilder : public Builder
+//{
+//public:
+//	void AddColor()
+//	{
+//	house->AddOrder("This is a white house");
+//	}
+//	void AddMaterial()
+//	{
+//		house->AddOrder("wood");
+//	}
+//	void AddFloor()
+//	{
+//		house->AddOrder("one-floor");
+//	}
+//	void AddWindow()
+//	{
+//		house->AddOrder("panoram window");
+//	}
+//};
+//
+//class StoneBuilder : public Builder
+//{
+//	void AddColor() override
+//	{
+//		house->AddOrder("This is grey house ");
+//	}
+//	void AddMaterial() override
+//	{
+//		house->AddOrder("stone");
+//	}
+//	void AddFloor() override
+//	{
+//		house->AddOrder("three-floor");
+//	}
+//	void AddWindow() override
+//	{
+//		house->AddOrder("enormous windows");
+//	}
+//};
+//
+//
+//class ChefManager
+//{
+//public:
+//	void CreateBigVilla(Builder* builder)
+//	{
+//		builder->AddColor();
+//		builder->AddMaterial();
+//		builder->AddFloor();
+//		builder->AddWindow();
+//	}
+//	void CreateLittleHouse(Builder* builder)
+//	{
+//		builder->AddColor();
+//		builder->AddFloor();
+//		builder->AddMaterial();
+//		builder->AddWindow();
+//	}
+//};
+//
