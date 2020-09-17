56ad1f8ce8c2ec958fed4984fd9d9cf179a7efce
vlasnata80@gmail.com <vlasnata80@gmail.com>
2020-09-17 23:04:42 +0300

    new

---
+++ Abstract Class Figure/Abstract Class Figure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abstract_Class_Figure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
